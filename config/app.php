dd6a45e1ae2f5657d689811d2d0918838269ac57
Natalie St Jean <natalie.stjean@gmail.com>
2019-09-17 14:13:19 -0400

    remove requirement for Debugbar

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Unisharp\Ckeditor\ServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -232,7 +231,6 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
